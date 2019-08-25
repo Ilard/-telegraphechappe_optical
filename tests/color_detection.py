7e6c815e1e0331b783e82e2812dc206d2cca5f00
JC_onLine <jc.gillot@gmail.com>
2019-08-25 10:20:10 +0200

    test(opencv): Color detection with realtime camera.

--- tests/color_detection.py
+++ tests/color_detection.py
@@ -3,18 +3,20 @@ import numpy as np
 
 def nothing(x):
     pass
-
+capture = cv2.VideoCapture(3)
 cv2.namedWindow("Tracking")
 cv2.createTrackbar("LowerHue", "Tracking", 0, 255, nothing)
-cv2.createTrackbar("LowerSaturation", "Tracking", 0, 255, nothing)
-cv2.createTrackbar("LowerValue", "Tracking", 0, 255, nothing)
 cv2.createTrackbar("UpperHue", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("LowerSaturation", "Tracking", 0, 255, nothing)
 cv2.createTrackbar("UpperSaturation", "Tracking", 255, 255, nothing)
+cv2.createTrackbar("LowerValue", "Tracking", 0, 255, nothing)
 cv2.createTrackbar("UpperValue", "Tracking", 255, 255, nothing)
 
 while True:
     # read image
-    frame = cv2.imread("img/smarties.png")
+    # frame = cv2.imread("img/smarties.png")
+    _, frame = capture.read()
+
     # lower range
     l_h = cv2.getTrackbarPos("LowerHue", "Tracking")
     l_s = cv2.getTrackbarPos("LowerSaturation", "Tracking")
@@ -23,7 +25,6 @@ while True:
     u_h = cv2.getTrackbarPos("UpperHue", "Tracking")
     u_s = cv2.getTrackbarPos("UpperSaturation", "Tracking")
     u_v = cv2.getTrackbarPos("UpperValue", "Tracking")
-
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS)
     l_b = np.array([l_h, l_s, l_v])   # lower blue color
@@ -39,6 +40,8 @@ while True:
     if key == 27:
         break
 
+# disconnect camera
+capture.release()
 # close window
 cv2.destroyAllWindows()
 
